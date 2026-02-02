c4f0b84940e5d184d846fb308e602cdc59f41b9b
Leelasiddharth <151850000+sidd-cr7@users.noreply.github.com>
2026-02-02 21:35:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: '/task-manager/',
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/task-manager/" : "/",
   plugins: [react()],
-})
+}));
